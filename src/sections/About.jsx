e181244f9d651e673ba407f12aa59e3be22f8a1c
PemapolS <pemapolsripratipbundit@gmail.com>
2024-11-26 13:15:30 +0700

    edit intro

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className="flex py-16 px-4 mx-auto max-w-screen-xl xl:py-8 lg:px-6 gap-4 items-center flex-col lg:flex-row">
         <div className="max-w-screen-md">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white font-ibm">Introduction</h2>
-            <p className="mb-8 text-gray-500 sm:text-lg dark:text-gray-400 font-ibm">Hi, I am Flowdy! I am an Information and Communication Engineering student from Thailand. <br />I dabble in Graphics Design, Web Design, and Photography in my free time.<br/>I am also a <a href="https://x.com/lupesuits">LupeSuits</a> suiter!</p>
+            <p className="mb-8 text-gray-500 sm:text-lg dark:text-gray-400 font-ibm">Hi, I am Flowdy! I am an Information and Communication Engineering student from Thailand. <br />I dabble in Graphics Design, Web Design, and Photography in my free time. I love traveling and meeting new people. Although, I can be a little shy so please be patient with me. I am also a <a href="https://x.com/lupesuits">LupeSuits</a> suiter!<br/>Thanks for sticking around!</p>
         </div>
         <div>
           <img 
