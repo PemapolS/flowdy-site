a097adc65466a7a0ec7be28aa35439c981b6bf7c
Pemapol Sripratipbundit <pemapolsripratipbundit@gmail.com>
2025-01-08 13:54:07 +0700

    add auto footer year change

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,4 +1,5 @@
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
     <section id="events" className="py-2 px-4 mx-auto max-w-screen-xl lg:px-6 gap-4 items-center">
       <p className="font-ibm text-white hidden lg:block">
@@ -10,7 +11,7 @@ const Footer = () => {
         Photos by <a href="https://fez.pet/">Fez</a>, <a href="https://x.com/Ryusuke6351">Ryusuke</a>
       </p>
       <p className="font-ibm text-white font-medium">
-        © 2024 Flowdy Husky
+        © {year} Flowdy Husky
       </p>
     </section>
   )
