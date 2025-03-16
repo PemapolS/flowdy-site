310c7fe85dde43b5a886ea03b355a7f53325c12e
Pemapol Sripratipbundit <pemapolsripratipbundit@gmail.com>
2025-03-16 23:52:26 +0700

    change hero img

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,11 +4,11 @@ const Footer = () => {
     <section id="events" className="py-2 px-4 mx-auto max-w-screen-xl lg:px-6 gap-4 items-center">
       <p className="font-ibm text-white hidden lg:block">
         Icon Art by Pastaiils | 
-        Photos by <a href="https://fez.pet/">Fez</a>, <a href="https://x.com/Ryusuke6351">Ryusuke</a>
+        Photos by <a href="https://x.com/callmenoop">Flamey</a>, <a href="https://x.com/Ryusuke6351">Ryusuke</a>
       </p>
       <p className="font-ibm text-white block lg:hidden">
         Icon Art by Pastaiils<br />
-        Photos by <a href="https://fez.pet/">Fez</a>, <a href="https://x.com/Ryusuke6351">Ryusuke</a>
+        Photos by <a href="https://x.com/callmenoop">Flamey</a>, <a href="https://x.com/Ryusuke6351">Ryusuke</a>
       </p>
       <p className="font-ibm text-white font-medium">
         © {year} Flowdy Husky
--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,7 @@ export default {
         '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
       },
       backgroundImage: {
-        'hero': "url('assets/images/big-photo1.jpg')",
+        'hero': "url('assets/images/big-photo2.jpg')",
         'card': "url('assets/images/thumbnail-background.svg')",
       },
       screens: {
