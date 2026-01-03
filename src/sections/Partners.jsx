9ba796dcba494dd4795e8a80f810069de522d724
PemapolS <138964662+PemapolS@users.noreply.github.com>
2026-01-03 13:27:23 +1100

    button recolor

--- src/sections/Partners.jsx
+++ src/sections/Partners.jsx
@@ -6,12 +6,12 @@ const Partners = () => {
       {/* Fez Partner Section */}
       <section 
         id="partners-fez"
-        className="w-full py-20 flex items-center justify-center bg-cover bg-center bg-no-repeat bg-blend-overlay bg-black/30"
+        className="w-full py-20 flex items-center justify-center bg-cover bg-center bg-no-repeat bg-blend-overlay dark:bg-black/30"
         style={{ backgroundImage: `url(${HB00_013_Banner.src})` }}
       >
         <div className="flex justify-center md:justify-end items-center max-w-3xl lg:max-w-5xl mx-auto w-full">
         <a href="https://fez.pet" className="group">
-          <div className="flex justify-center flex-col px-5 py-5 lg:px-8 rounded-xl bg-white/0 dark:bg-transparent ring-1 ring-slate-900/10 dark:ring-0 transition-transform duration-200 ease-out transform hover:scale-[1.02] hover:shadow-lg hover:bg-slate-100 dark:hover:bg-slate-700/40 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-purple-500">
+          <div className="flex justify-center flex-col px-5 py-5 lg:px-8 rounded-xl bg-white/30 dark:bg-transparent ring-1 ring-slate-900/10 dark:ring-0 transition-transform duration-200 ease-out transform hover:scale-[1.02] hover:shadow-lg hover:bg-slate-100 dark:hover:bg-slate-700/40 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-purple-500">
             <img 
               src={Fez.src}
               alt="Fez"
@@ -28,12 +28,12 @@ const Partners = () => {
       {/* BlueJay Partner Section */}
       <section 
         id="partners-bluejay"
-        className="w-full py-20 flex items-center justify-center bg-cover bg-center bg-no-repeat bg-blend-overlay bg-black/40"
+        className="w-full py-20 flex items-center justify-center bg-cover bg-center bg-no-repeat bg-blend-overlay dark:bg-black/30"
         style={{ backgroundImage: `url(${FI00_004.src})` }}
       >
       <div className="flex justify-center md:justify-start items-center max-w-3xl lg:max-w-5xl mx-auto w-full">
         <a href="https://linktr.ee/BlueJaytheWolf" className="group">
-          <div className="flex justify-center flex-col px-5 py-5 lg:px-8 rounded-xl bg-white/0 dark:bg-transparent ring-1 ring-slate-900/10 dark:ring-0 transition-transform duration-200 ease-out transform hover:scale-[1.02] hover:shadow-lg hover:bg-slate-100 dark:hover:bg-slate-700/40 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-purple-500">
+          <div className="flex justify-center flex-col px-5 py-5 lg:px-8 rounded-xl bg-white/30 dark:bg-transparent ring-1 ring-slate-900/10 dark:ring-0 transition-transform duration-200 ease-out transform hover:scale-[1.02] hover:shadow-lg hover:bg-slate-100 dark:hover:bg-slate-700/40 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-purple-500">
             <img 
               src={BlueJay.src}
               alt="BlueJay"
